55e3efffbf860bce7dc502551a0cb8a2c7003284
luncliff <luncliff@gmail.com>
2026-03-15 19:03:15 +0900

    Add glob test harness and update Jest configuration for glob module

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,22 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  moduleNameMapper: {
-    '^@actions/core$': '<rootDir>/__tests__/helpers/action-test-harness.ts'
-  },
-  collectCoverage: false,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text'],
-  collectCoverageFrom: ['src/**/*.ts', '!src/CodeCoverageSummary/**'],
-  coverageThreshold: {
-    global: {
-      lines: 80,
-      branches: 80,
-      functions: 80,
-      statements: 80
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.test.ts'],
+    moduleNameMapper: {
+        '^@actions/core$': '<rootDir>/__tests__/helpers/action-test-harness.ts',
+        '^@actions/glob$': '<rootDir>/__tests__/helpers/glob-test-harness.ts'
+    },
+    collectCoverage: false,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['lcov', 'text'],
+    collectCoverageFrom: ['src/**/*.ts', '!src/CodeCoverageSummary/**'],
+    coverageThreshold: {
+        global: {
+            lines: 80,
+            branches: 80,
+            functions: 80,
+            statements: 80
+        }
     }
-  }
 }
